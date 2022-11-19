0675f6e44573d6eda80ec5316fdf626414d6b5eb
wiirhan <wiirhan@163.com>
2022-11-19 16:24:56 +0800

    feat: add Editor

---
+++ src/components/Editor/TheEditor.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Vditor from "vditor";
+import { onMounted, ref } from "vue";
+import "vditor/dist/index.css";
+
+const props = withDefaults(
+    defineProps<{
+        id: string;
+        options?: Record<string, any>;
+    }>(),
+    { id: "vditor" }
+);
+
+const vditor = ref<Vditor | null>(null);
+
+onMounted(() => {
+    vditor.value = new Vditor(props.id, {
+        after: () => {
+            // vditor.value is a instance of Vditor now and thus can be safely used here
+            vditor.value!.setValue(
+                "Vue Composition API + Vditor + TypeScript Minimal Example"
+            );
+        },
+        ...props.options,
+    });
+});
+</script>
+
+<template>
+    <div :id="props.id" />
+</template>
